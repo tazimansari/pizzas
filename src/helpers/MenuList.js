846fda8c5a8c9cba69d859c0c792c66e11d15055
Tazim Ansari <tazimansari1999@gmail.com>
2022-09-16 19:41:51 +0530

    price change

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -9,27 +9,27 @@ export const MenuList = [
   {
     name: "Pepperoni Pizza",
     image: Pepperoni,
-    price: 15.99,
+    price: 1500.99,
   },
   {
     name: "Margherita Pizza",
     image: Margherita,
-    price: 11.99,
+    price: 1001.99,
   },
   {
     name: "PedroTech Special Pizza",
     image: PedroTechSpecial,
-    price: 25.53,
+    price: 250.53,
   },
   {
     name: "Vegan Pizza",
     image: Vegan,
-    price: 17.99,
+    price: 1107.99,
   },
   {
     name: "Pineapple Pizza",
     image: Pineapple,
-    price: 4.99,
+    price: 400.99,
   },
   {
     name: "Very Expensive Pizza",
